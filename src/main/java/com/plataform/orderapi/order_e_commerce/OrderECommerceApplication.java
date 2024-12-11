60d3a2547d30c6dbebe10d435d3696961380f356
MiguelFuquene1024 <migueplay_10@hotmail.com>
2024-12-11 17:56:33 -0500

    Initial Structure

---
+++ src/main/java/com/plataform/orderapi/order_e_commerce/OrderECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.plataform.orderapi.order_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plataform/orderapi/order_e_commerce/OrderECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataform.orderapi.order_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
